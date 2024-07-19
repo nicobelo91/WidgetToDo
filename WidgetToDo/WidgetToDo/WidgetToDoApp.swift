f4e2d4e5f08655884da67827b32e951c26c506be
Nicolas Cobelo <nicolascobelo@MacBook-Pro-2.local>
2024-07-19 13:22:47 -0300

    Create Xcode project

---
+++ WidgetToDo/WidgetToDo/WidgetToDoApp.swift
@@ -0,0 +1,17 @@
+//
+//  WidgetToDoApp.swift
+//  WidgetToDo
+//
+//  Created by Nicolas Cobelo on 19/07/2024.
+//
+
+import SwiftUI
+
+@main
+struct WidgetToDoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
